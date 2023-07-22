b611f257efbffeff41ddb646162a1c9d9375ef80
Ryan Tan <4nimefan@gmail.com>
2023-07-22 12:51:19 -0700

    ./models

---
+++ portfolio/src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import StartScreen from './components/StartScreen.vue'
+
+export default {
+  components: {
+    StartScreen,
+  },
+  data() {
+    return {
+      showStartScreen: false,
+    };
+  },
+  watch: {
+    '$route'(to, from) {
+      // Trigger the transition effect when navigating to a new route
+      this.showStartScreen = true;
+    },
+  },
+  methods: {
+    handleTransitionEnd() {
+      // Reset the flag after the transition ends
+      this.showStartScreen = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <StartScreen msg="Vite + Vue" />
+</template>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ portfolio/src/components/StartScreen.vue
@@ -0,0 +1,46 @@
+<template>
+    <div ref="canvasContainer"></div>
+</template>
+  
+<script>
+    import * as THREE from 'three';
+    var cube;
+    var scene;
+    var camera;
+    var renderer;
+    init();
+    animate();
+
+    function init()
+    {
+        window.addEventListener('resize', resize);
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000);
+        camera.position.z = 10;
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+    }
+        
+    function resize()
+    {
+        renderer.setSize( window.innerWidth, window.innerHeight);
+    }
+    
+    function animate() {
+        requestAnimationFrame( animate );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render( scene, camera );
+    }
+</script>
+  
